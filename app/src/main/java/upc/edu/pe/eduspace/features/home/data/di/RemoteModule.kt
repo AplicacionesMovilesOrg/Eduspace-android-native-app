bde9faba4874168478db403bc429b4658c105428
vquijandria <u201822697@upc.edu.pe>
2025-10-10 14:57:14 -0500

    feat: added RemoteModule.kt component

---
+++ app/src/main/java/upc/edu/pe/eduspace/features/home/data/di/RemoteModule.kt
@@ -0,0 +1,19 @@
+package upc.edu.pe.eduspace.features.home.data.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import upc.edu.pe.eduspace.features.home.data.remote.services.HomeService
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteModule {
+
+    @Provides
+    @Singleton
+    fun provideHomeService(retrofit: Retrofit): HomeService =
+        retrofit.create(HomeService::class.java)
+}
