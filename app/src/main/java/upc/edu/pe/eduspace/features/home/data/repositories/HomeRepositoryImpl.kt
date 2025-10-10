24ec56e5a351cc9d639b70fae19543dde89700d0
vquijandria <u201822697@upc.edu.pe>
2025-10-10 14:56:42 -0500

    feat: added HomeRepositoryImpl.kt component

--- app/src/main/java/upc/edu/pe/eduspace/features/home/data/repositories/HomeRepositoryImpl.kt
+++ app/src/main/java/upc/edu/pe/eduspace/features/home/data/repositories/HomeRepositoryImpl.kt
@@ -1,17 +1,24 @@
-package upc.edu.pe.eduspace.features.home.data.repositories
+package upc.edu.pe.eduspace.features.home.data.remote.repositories
 
-import kotlinx.coroutines.delay
+import upc.edu.pe.eduspace.features.home.data.remote.services.HomeService
+import upc.edu.pe.eduspace.features.home.data.remote.models.toDomain
+import upc.edu.pe.eduspace.features.home.domain.models.ReportResource
 import upc.edu.pe.eduspace.features.home.domain.models.UserHome
 import upc.edu.pe.eduspace.features.home.domain.repositories.HomeRepository
 import javax.inject.Inject
 
-class HomeRepositoryImpl @Inject constructor() : HomeRepository {
+class HomeRepositoryImpl @Inject constructor(
+    private val homeService: HomeService
+) : HomeRepository {
+
     override suspend fun getUserHome(): UserHome {
-        delay(1000) // simular llamada remota
+        val reports = homeService.getReports().map { it.toDomain() }
+
+        // Por ahora el nombre del usuario lo podrías obtener del token o de authRepo
         return UserHome(
             firstName = "Andres",
             lastName = "Torres",
-            hasReports = false
+            reports = reports
         )
     }
 }
