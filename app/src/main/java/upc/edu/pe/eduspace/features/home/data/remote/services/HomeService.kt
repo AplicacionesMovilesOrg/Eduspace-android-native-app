cb29c74f13a35b73b99070f0fc5a3c79296ed350
vquijandria <u201822697@upc.edu.pe>
2025-10-11 02:58:01 -0500

    refactor: fixed endpoints from reports and administrator-profiles api

--- app/src/main/java/upc/edu/pe/eduspace/features/home/data/remote/services/HomeService.kt
+++ app/src/main/java/upc/edu/pe/eduspace/features/home/data/remote/services/HomeService.kt
@@ -2,8 +2,12 @@ package upc.edu.pe.eduspace.features.home.data.remote.services
 
 import retrofit2.http.GET
 import upc.edu.pe.eduspace.features.home.data.remote.models.ReportDto
-
+import upc.edu.pe.eduspace.features.home.data.remote.models.AdministratorProfileDto
 interface HomeService {
-    @GET("http://10.0.2.2:8080/api/v1/reports\n")
+    @GET("reports")
     suspend fun getReports(): List<ReportDto>
+
+    @GET("administrator-profiles")
+    suspend fun getAdministratorProfiles(): List<AdministratorProfileDto>
+
 }
