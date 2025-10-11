c0f5c9bfdc1127e31e4fc53634a63bda6214137a
vquijandria <u201822697@upc.edu.pe>
2025-10-11 02:16:24 -0500

    feat: added get reports endpoint to the HomeService

--- app/src/main/java/upc/edu/pe/eduspace/features/home/data/remote/services/HomeService.kt
+++ app/src/main/java/upc/edu/pe/eduspace/features/home/data/remote/services/HomeService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 import upc.edu.pe.eduspace.features.home.data.remote.models.ReportDto
 
 interface HomeService {
-    @GET("reports") // ← endpoint de tu API
+    @GET("http://10.0.2.2:8080/api/v1/reports\n")
     suspend fun getReports(): List<ReportDto>
 }
