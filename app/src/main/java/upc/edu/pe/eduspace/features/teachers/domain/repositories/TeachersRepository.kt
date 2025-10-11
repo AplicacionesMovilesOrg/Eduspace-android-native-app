4696bebcc57733e466c80b2272c6f39b4714ee40
AntonioNavarro24 <137426429+AntonioNavarro24@users.noreply.github.com>
2025-10-11 04:22:53 -0500

    feat(teachers): add createTeacher method to repository

--- app/src/main/java/upc/edu/pe/eduspace/features/teachers/domain/repositories/TeachersRepository.kt
+++ app/src/main/java/upc/edu/pe/eduspace/features/teachers/domain/repositories/TeachersRepository.kt
@@ -4,4 +4,11 @@ import upc.edu.pe.eduspace.features.teachers.domain.model.Teacher
 
 interface TeachersRepository {
     suspend fun getAllTeachers(): List<Teacher>
+
+    suspend fun createTeacher(input: CreateTeacher): Teacher?
 }
+
+data class CreateTeacher(
+    val firstName: String, val lastName: String, val email: String, val dni: String,
+    val address: String, val phone: String, val username: String, val password: String
+)
