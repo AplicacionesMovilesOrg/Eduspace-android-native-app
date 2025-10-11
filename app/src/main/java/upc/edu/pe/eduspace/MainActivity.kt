0342111280cf1aaff437e8384ef668dcf6cd3e45
AntonioNavarro24 <137426429+AntonioNavarro24@users.noreply.github.com>
2025-10-10 23:22:51 -0500

    feat(core-root): integrate teachers navigation entry

--- app/src/main/java/upc/edu/pe/eduspace/MainActivity.kt
+++ app/src/main/java/upc/edu/pe/eduspace/MainActivity.kt
@@ -4,44 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import upc.edu.pe.eduspace.core.ui.theme.EduspaceTheme
+import dagger.hilt.android.AndroidEntryPoint
+import upc.edu.pe.eduspace.core.navigation.AppNav
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            EduspaceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppNav()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EduspaceTheme {
-        Greeting("Android")
     }
 }
