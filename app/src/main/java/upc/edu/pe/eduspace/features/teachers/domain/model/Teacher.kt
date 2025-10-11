429ad297c6e238dca06ffc4dfea6521408062dae
AntonioNavarro24 <137426429+AntonioNavarro24@users.noreply.github.com>
2025-10-10 23:24:24 -0500

    feat(teachers-domain-model): create Teachers entity model

---
+++ app/src/main/java/upc/edu/pe/eduspace/features/teachers/domain/model/Teacher.kt
@@ -0,0 +1,9 @@
+package upc.edu.pe.eduspace.features.teachers.domain.model
+
+data class Teacher(
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val dni: String
+)
+
