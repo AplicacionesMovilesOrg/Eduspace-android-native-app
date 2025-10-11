c1370e3dc04ec6aa3e74f24c1198d9a16237c5e4
AntonioNavarro24 <137426429+AntonioNavarro24@users.noreply.github.com>
2025-10-10 23:26:08 -0500

    feat(teachers-ui): define UiState for teachers presentation layer

---
+++ app/src/main/java/upc/edu/pe/eduspace/core/utils/UiState.kt
@@ -0,0 +1,8 @@
+package upc.edu.pe.eduspace.core.utils
+
+sealed class UiState<out T> {
+    object Initial: UiState<Nothing>()
+    object Loading: UiState<Nothing>()
+    data class Success<T>(val data: T): UiState<T>()
+    data class Error(val message: String): UiState<Nothing>()
+}
