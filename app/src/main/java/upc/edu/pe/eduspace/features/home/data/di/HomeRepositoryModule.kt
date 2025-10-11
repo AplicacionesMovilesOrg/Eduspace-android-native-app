5fa6efe6b41ebd695613286c47463d601ce934b5
vquijandria <u201822697@upc.edu.pe>
2025-10-11 02:57:40 -0500

    refactor: renamed the HomeRepositoryModule.kt from RepositoryModule to HomeRepositoryModule

--- app/src/main/java/upc/edu/pe/eduspace/features/home/data/di/HomeRepositoryModule.kt
+++ app/src/main/java/upc/edu/pe/eduspace/features/home/data/di/HomeRepositoryModule.kt
@@ -5,12 +5,12 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import upc.edu.pe.eduspace.features.home.domain.repositories.HomeRepository
-import upc.edu.pe.eduspace.features.home.data.remote.repositories.HomeRepositoryImpl
+import upc.edu.pe.eduspace.features.home.data.repositories.HomeRepositoryImpl
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class RepositoryModule {
+abstract class HomeRepositoryModule {
 
     @Binds
     @Singleton
