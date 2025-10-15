e57ff7c081437850f5fd43a22fcab6ce21feb54f
Andres Torres <andresalbertotorresgarcia@gmail.com>
2025-10-15 13:57:44 -0500

    fix(classrooms): Update import statement for hiltViewModel to use lifecycle package

--- app/src/main/java/upc/edu/pe/eduspace/features/classrooms/presentation/classrooms/Classrooms.kt
+++ app/src/main/java/upc/edu/pe/eduspace/features/classrooms/presentation/classrooms/Classrooms.kt
@@ -32,7 +32,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import upc.edu.pe.eduspace.core.utils.UiState
 import upc.edu.pe.eduspace.features.classrooms.domain.models.Classroom
 import upc.edu.pe.eduspace.features.classrooms.presentation.classrooms.components.ClassroomCard
