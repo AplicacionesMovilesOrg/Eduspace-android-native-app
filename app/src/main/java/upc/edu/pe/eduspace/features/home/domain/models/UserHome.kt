18689654a888e71f07fb741f53b573d720e09b70
vquijandria <u201822697@upc.edu.pe>
2025-11-13 21:40:22 -0500

    refactor: added displayName in UserHome class

--- app/src/main/java/upc/edu/pe/eduspace/features/home/domain/models/UserHome.kt
+++ app/src/main/java/upc/edu/pe/eduspace/features/home/domain/models/UserHome.kt
@@ -3,5 +3,6 @@ package upc.edu.pe.eduspace.features.home.domain.models
 data class UserHome(
     val firstName: String,
     val lastName: String,
+    val displayName: String,
     val reports: List<ReportResource>
 )
