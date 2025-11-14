70ab0b9a153de2e2e08149a8c7625997850d0afe
vquijandria <u201822697@upc.edu.pe>
2025-11-13 21:41:59 -0500

    refactor: added USERNAME_KEY for the username in the SessionManager.kt

--- app/src/main/java/upc/edu/pe/eduspace/core/data/SessionManager.kt
+++ app/src/main/java/upc/edu/pe/eduspace/core/data/SessionManager.kt
@@ -23,25 +23,22 @@ class SessionManager @Inject constructor(@param:ApplicationContext private val c
         val IS_LOGGED_IN_KEY = booleanPreferencesKey("is_logged_in")
         val USER_EMAIL_KEY = stringPreferencesKey("user_email")
         val AUTH_TOKEN_KEY = stringPreferencesKey("auth_token")
+        val USERNAME_KEY = stringPreferencesKey("username")
     }
 
-    suspend fun saveSession(adminId: String, email: String, token: String) {
+
+    suspend fun saveSession(adminId: String, email: String, token: String, username: String) {
         context.dataStore.edit { preferences ->
             preferences[ADMIN_ID_KEY] = adminId
             preferences[IS_LOGGED_IN_KEY] = true
             preferences[USER_EMAIL_KEY] = email
             preferences[AUTH_TOKEN_KEY] = token
+            preferences[USERNAME_KEY] = username
         }
     }
 
     val authTokenFlow: Flow<String?> = context.dataStore.data.map { preferences ->
         preferences[AUTH_TOKEN_KEY]
-    }
-
-    suspend fun clearSession() {
-        context.dataStore.edit { preferences ->
-            preferences.clear()
-        }
     }
 
     val adminIdFlow: Flow<String?> = context.dataStore.data.map { preferences ->
@@ -51,5 +48,20 @@ class SessionManager @Inject constructor(@param:ApplicationContext private val c
     val isLoggedInFlow: Flow<Boolean> = context.dataStore.data.map { preferences ->
         preferences[IS_LOGGED_IN_KEY] ?: false
     }
-}
 
+
+    val userEmailFlow: Flow<String?> = context.dataStore.data.map { preferences ->
+        preferences[USER_EMAIL_KEY]
+    }
+
+
+    val usernameFlow: Flow<String?> = context.dataStore.data.map { preferences ->
+        preferences[USERNAME_KEY]
+    }
+
+    suspend fun clearSession() {
+        context.dataStore.edit { preferences ->
+            preferences.clear()
+        }
+    }
+}
